a2b5d198129273b19daedaef69d76ff27006f39c
Matias Ezequiel Vara Larsen <mvaralar@redhat.com>
2024-04-29 16:46:41 +0200

    Add launch test for arm CCA
    This is a WIP commit.
    
    Signed-off-by: Matias Ezequiel Vara Larsen <mvaralar@redhat.com>

--- src/error.rs
+++ src/error.rs
@@ -5,4 +5,10 @@ use vmm_sys_util::errno;
 #[derive(Debug)]
 pub enum Error {
     VmCreate(errno::Error),
+    GICCreate(errno::Error),
+    GICInit(errno::Error),
+    Config(errno::Error),
+    RDCreate(errno::Error),
+    RPopulate(errno::Error),
+    RActivate(errno::Error)
 }
